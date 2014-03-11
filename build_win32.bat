e2358969aa4b9702060e76de37d86d617eda4218
Markus Mohr <mohr@markus-mohr.com>
2014-03-11 14:19:24 +0100

    added simple win batch files to build cryptopp

---
+++ build_win32.bat
@@ -0,0 +1,2 @@
+msbuild.exe /p:Configuration=Debug /p:Platform=Win32 cryptlib.vcxproj
+msbuild.exe /p:Configuration=Release /p:Platform=Win32 cryptlib.vcxproj
---
+++ build_win64.bat
@@ -0,0 +1,2 @@
+msbuild.exe /p:Configuration=Debug /p:Platform=x64 cryptlib.vcxproj
+msbuild.exe /p:Configuration=Release /p:Platform=x64 cryptlib.vcxproj
