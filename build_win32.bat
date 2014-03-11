7fc3179b41368ed55140ddec715e02484196476d
Markus Mohr <mohr@markus-mohr.com>
2014-03-11 14:40:19 +0100

    added clean step to buidl scripts and removed adhoc.cpp.copied

--- build_win32.bat
+++ build_win32.bat
@@ -1,2 +1,5 @@
+msbuild.exe /p:Configuration=Debug /p:Platform=Win32 /t:clean cryptlib.vcxproj
+msbuild.exe /p:Configuration=Release /p:Platform=Win32 /t:clean cryptlib.vcxproj
+
 msbuild.exe /p:Configuration=Debug /p:Platform=Win32 cryptlib.vcxproj
 msbuild.exe /p:Configuration=Release /p:Platform=Win32 cryptlib.vcxproj
--- build_win64.bat
+++ build_win64.bat
@@ -1,2 +1,5 @@
+msbuild.exe /p:Configuration=Debug /p:Platform=x64 /t:clean cryptlib.vcxproj
+msbuild.exe /p:Configuration=Release /p:Platform=x64 /t:clean cryptlib.vcxproj
+
 msbuild.exe /p:Configuration=Debug /p:Platform=x64 cryptlib.vcxproj
 msbuild.exe /p:Configuration=Release /p:Platform=x64 cryptlib.vcxproj
