c1f68340b6f9370bedfb83214ce5901435f46bdf
Marcus Ilgner <marcus@doo.net>
2014-04-11 17:23:55 +0200

    Fix typo in build script

--- build.vs2012.bat
+++ build.vs2012.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SETLOCAL EnableDelayedExpansion EnableExtension
+SETLOCAL EnableDelayedExpansion EnableExtensions
 call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x64
 
 pushd %~dp0
